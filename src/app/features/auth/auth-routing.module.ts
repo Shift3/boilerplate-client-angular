f0943912f945b3c2e1302e84a43d5536f93babbc
Elanna Grossman <elanna.grossman@gmail.com>
2020-06-01 11:17:13 -0700

    feat(auth): add `AuthRoutingModule` and wire up `AuthLayoutComponent`

---
+++ src/app/features/auth/auth-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthLayoutComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AuthRoutingModule { }
+
+export const components = [
+  AuthLayoutComponent,
+];
