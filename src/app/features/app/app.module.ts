478a95ed3bc1793876ecf4d92d3939f5ee7dcd09
Elanna Grossman <elanna.grossman@gmail.com>
2020-06-01 13:23:53 -0700

    chore(app): wire up `HttpClientModule`

--- src/app/features/app/app.module.ts
+++ src/app/features/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,6 +13,7 @@ import { CoreModule } from '@core/core.module';
   imports: [
     BrowserModule,
     CoreModule,
+    HttpClientModule,
     // AppRoutingModule must be loaded last.
     AppRoutingModule,
   ],
