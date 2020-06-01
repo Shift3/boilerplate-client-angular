41e495203d65545c45b5bb94bf7639a782a07c0b
Elanna Grossman <elanna.grossman@gmail.com>
2020-06-01 13:14:01 -0700

    feat(core): wire up `SharedModule`

--- src/app/infrastructure/core/core.module.ts
+++ src/app/infrastructure/core/core.module.ts
@@ -5,11 +5,16 @@ import {
 } from '@angular/core';
 
 import { ModuleImportGuard } from './guards/module-import.guard';
+import { SharedModule } from '@shared/shared.module';
 
 /**
  * Connects services, resolvers, guards, and more that must be singleton instances.
  */
-@NgModule({ })
+@NgModule({
+  imports: [
+    SharedModule,
+  ],
+})
 export class CoreModule {
   constructor( @Optional() @SkipSelf() parentModule: CoreModule) {
     ModuleImportGuard.throwIfAlreadyLoaded(parentModule, 'CoreModule');
