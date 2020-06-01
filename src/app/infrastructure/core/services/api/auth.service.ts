201ae87cda525a55337b88bf80a7bbe449bd4d78
Elanna Grossman <elanna.grossman@gmail.com>
2020-06-01 13:33:43 -0700

    feat(auth): add initial `login()` and `logout()` methods

--- src/app/infrastructure/core/services/api/auth.service.ts
+++ src/app/infrastructure/core/services/api/auth.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from '@angular/core';
 
+import { Observable } from 'rxjs';
+
 import { ApiService } from './api.service';
+import { environment } from '@env/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  private url: string = `${environment.apiRoute}`;
+
   constructor(
     private apiService: ApiService,
   ) {  }
+
+  public login(payload: unknown): Observable<unknown> {
+    const endpoint = `${this.url}/login/`;
+    return this.apiService.post<unknown, unknown>(endpoint, payload);
+  }
+
+  public logout(): void {
+    sessionStorage.clear();
+  }
 }
