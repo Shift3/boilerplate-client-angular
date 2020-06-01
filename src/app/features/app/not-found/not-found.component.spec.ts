00f72529110cf088f40504be71ad68d17dc831e5
Elanna Grossman <elanna.grossman@gmail.com>
2020-06-01 12:30:25 -0700

    chore, test(app, auth): wrap tests in unit/integration check

--- src/app/features/app/not-found/not-found.component.spec.ts
+++ src/app/features/app/not-found/not-found.component.spec.ts
@@ -1,27 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { environment } from '@env/environment.test';
+import { Logger } from '@utils/logger';
 import { NotFoundComponent } from './not-found.component';
 
-describe('NotFoundComponent', () => {
-  let component: NotFoundComponent;
-  let fixture: ComponentFixture<NotFoundComponent>;
+!environment.testIntegration
+  ? Logger.log('Integration skipped')
+  : describe('[Integration] NotFoundComponent', () => {
+    let component: NotFoundComponent;
+    let fixture: ComponentFixture<NotFoundComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        NotFoundComponent,
-       ],
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [
+          NotFoundComponent,
+        ],
+      })
+      .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(NotFoundComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    beforeEach(() => {
+      fixture = TestBed.createComponent(NotFoundComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
