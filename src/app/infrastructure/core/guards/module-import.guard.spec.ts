6b3072574ab6a3f2b0900b6c5f533311f80a538b
Elanna Grossman <elanna.grossman@gmail.com>
2020-06-01 12:35:41 -0700

    style(module-import): adjust line breaks

--- src/app/infrastructure/core/guards/module-import.guard.spec.ts
+++ src/app/infrastructure/core/guards/module-import.guard.spec.ts
@@ -17,10 +17,12 @@ import { ModuleImportGuard } from './module-import.guard';
     describe('throwIfAlreadyLoaded()', () => {
       it(`does nothing when not passed a CoreModule`, () => {
         const noModule: CoreModule = null;
-        const moduleName = '';				expect(ModuleImportGuard.throwIfAlreadyLoaded(noModule, moduleName)).toBeUndefined();
+        const moduleName = '';
+        expect(ModuleImportGuard.throwIfAlreadyLoaded(noModule, moduleName)).toBeUndefined();
       });
       it(`throws an error when passed a second CoreModule`, () => {
-        const moduleName = 'TestCoreModule';				expect(() => {
+        const moduleName = 'TestCoreModule';
+        expect(() => {
           ModuleImportGuard.throwIfAlreadyLoaded(coreModule, moduleName);
         }).toThrowError(`${moduleName} has already been loaded. Import Core modules in the AppModule only.`);
       });
