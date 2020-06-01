d71682a1e07eca905566370e4cb071809cc3b6c4
Elanna Grossman <elanna.grossman@gmail.com>
2020-06-01 11:16:23 -0700

    feat(auth): add `AuthLayoutComponent`

---
+++ src/app/features/auth/auth-layout/auth-layout.component.spec.ts
@@ -0,0 +1,29 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { AuthLayoutComponent } from './auth-layout.component';
+
+describe('AuthLayoutComponent', () => {
+  let component: AuthLayoutComponent;
+  let fixture: ComponentFixture<AuthLayoutComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AuthLayoutComponent,
+      ],
+      imports: [ RouterTestingModule ],
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AuthLayoutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
