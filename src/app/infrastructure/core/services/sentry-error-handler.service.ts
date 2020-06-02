fe1676e083c19e370f11a833b062f371dddaa061
Elanna Grossman <elanna.grossman@gmail.com>
2020-06-02 01:49:57 -0700

    feat(services): add `SentryErrorHandlerService`

---
+++ src/app/infrastructure/core/services/sentry-error-handler.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+
+import * as Sentry from '@sentry/browser';
+
+import { environment } from '@env/environment';
+import { version } from '../../../../version';
+
+Sentry.init({
+  dsn: environment.sentry.DSN,
+  environment: environment.name,
+  release: version,
+  // TODO: Evaluate for false negatives.
+  integrations(integrations) {
+    return integrations.filter((i) => i.name !== 'TryCatch');
+  },
+});
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SentryErrorHandlerService {
+  public handleError(
+    error: any,
+    showDialog: boolean = false,
+    errorMessageList: string[] = [],
+  ): void {
+    Sentry.configureScope((scope) => {
+      if (errorMessageList.length) {
+        scope.setExtra('Error Context', errorMessageList[0]);
+      }
+    });
+    const eventId = Sentry.captureException(error.originalError || error);
+    if (showDialog && environment.sentry.dialogEnabled) {
+      Sentry.showReportDialog({ eventId });
+    }
+  }
+}
