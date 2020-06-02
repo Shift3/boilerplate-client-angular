4c15e32fa2b0f7c7f39a8b5e3c88bf54169f315c
Elanna Grossman <elanna.grossman@gmail.com>
2020-06-02 12:47:05 -0700

    chore(environments): add environment name for sentry logging

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  name: 'Production',
   apiRoute: '',
   sentry: {
     DSN: '',
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  name: 'Staging',
   apiRoute: '',
   sentry: {
     DSN: '',
