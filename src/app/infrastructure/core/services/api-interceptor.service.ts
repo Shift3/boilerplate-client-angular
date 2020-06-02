c991bb9da2d3b5a99312feac67d967ef025a89ec
Elanna Grossman <elanna.grossman@gmail.com>
2020-06-02 02:42:33 -0700

    feat(api-interceptor): wire up error handling to interceptor

--- src/app/infrastructure/core/services/api-interceptor.service.ts
+++ src/app/infrastructure/core/services/api-interceptor.service.ts
@@ -16,9 +16,15 @@ import {
   retry,
 } from 'rxjs/operators';
 
+import { GlobalErrorHandlerService } from './global-error-handler.service';
+
 @Injectable()
 export class ApiInterceptorService implements HttpInterceptor {
   private AUTH_HEADER = 'Authorization';
+
+  constructor(
+    private globalErrorHandlerService: GlobalErrorHandlerService,
+  ) { }
 
   intercept<T>(req: HttpRequest<T>, next: HttpHandler): Observable<HttpEvent<T>> {
     if (!req.headers.has('Content-Type')) {
@@ -29,9 +35,26 @@ export class ApiInterceptorService implements HttpInterceptor {
     req = this.addAuthenticationToken(req);
 
     return next.handle(req).pipe(
-      retry(1),
-      catchError((error: HttpErrorResponse) => observableThrowError(error),
-    ));
+      catchError((error: HttpErrorResponse) => {
+        switch (error.status) {
+          case 0:
+          case 500:
+            this.globalErrorHandlerService.handleError(error);
+            break;
+          case 401:
+            // TODO: Add retry logic
+            this.globalErrorHandlerService.handleError(error);
+            break;
+          case 403:
+            this.globalErrorHandlerService.handleError(error);
+            break;
+          default:
+            break;
+        }
+
+        return observableThrowError(error);
+      }),
+    );
   }
 
   private addAuthenticationToken<T>(request: HttpRequest<T>): HttpRequest<T> {
