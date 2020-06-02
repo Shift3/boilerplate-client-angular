ea5e25bd9c135dd9cba2c93ffa58c761b82bfe39
Elanna Grossman <elanna.grossman@gmail.com>
2020-06-02 00:36:58 -0700

    feat, test(login): add `LoginSmartComponent`, `LoginPresentationComponent` and tests

---
+++ src/app/features/auth/login/login-presentation.component.ts
@@ -0,0 +1,34 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
+
+import { IFormConfig } from '@models/form/form';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-presentation',
+  templateUrl: './login-presentation.component.html',
+  styleUrls: ['./login-presentation.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LoginPresentationComponent {
+  @Input() public formConfig: IFormConfig;
+
+  @Output() public emitForm = new EventEmitter<FormGroup>();
+  @Output() public emitSubmit = new EventEmitter<void>();
+
+  public formTitle: string = 'Log In';
+
+  public propagateForm(form: FormGroup): void {
+    this.emitForm.emit(form);
+  }
+
+  public submit(): void {
+    this.emitSubmit.emit();
+  }
+
+}
---
+++ src/app/features/auth/login/login-smart.component.spec.ts
@@ -0,0 +1,43 @@
+import {
+  HttpClientTestingModule,
+} from '@angular/common/http/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { environment } from '@env/environment.test';
+import { Logger } from '@utils/logger';
+import { LoginPresentationComponent } from './login-presentation.component';
+import { LoginSmartComponent } from './login-smart.component';
+
+!environment.testIntegration
+  ? Logger.log('Integration skipped')
+  : describe('[Integration] LoginSmartComponent', () => {
+    let component: LoginSmartComponent;
+    let fixture: ComponentFixture<LoginSmartComponent>;
+
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [
+          LoginPresentationComponent,
+          LoginSmartComponent,
+        ],
+        imports: [
+          ReactiveFormsModule,
+          HttpClientTestingModule,
+          RouterTestingModule,
+        ],
+      })
+      .compileComponents();
+    }));
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(LoginSmartComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+  });
