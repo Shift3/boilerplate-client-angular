7ad13078609875af828658374edeff6bfb6e150b
Elanna Grossman <elanna.grossman@gmail.com>
2020-06-02 01:49:08 -0700

    chore(environments): add sentry environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: true,
   apiRoute: '',
+  sentry: {
+    DSN: '',
+    enabled: true,
+    dialogEnabled: true,
+  },
 };
--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,6 +1,11 @@
 export const environment = {
   production: false,
   apiRoute: '',
+  sentry: {
+    DSN: '',
+    enabled: true,
+    dialogEnabled: true,
+  },
   testUnit: true,
   testIntegration: true,
 };
