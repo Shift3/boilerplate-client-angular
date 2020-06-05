20e26fd56a3916057b684006ca4aac5e58929904
Elanna Grossman <elanna.grossman@gmail.com>
2020-06-05 15:29:39 -0700

    refactor(user): adjust return signature

--- src/app/infrastructure/core/services/api/user.service.ts
+++ src/app/infrastructure/core/services/api/user.service.ts
@@ -6,6 +6,7 @@ import { ApiService } from './api.service';
 import { environment } from '@env/environment';
 import {
   IForgotPasswordRequest,
+  IForgotPasswordDTO,
   IResetPasswordRequest,
 } from '@models/user';
 
@@ -22,13 +23,13 @@ export class UserService {
     this.url = `${environment.apiRoute}/${this.controllerRoute}`;
   }
 
-  public forgotPassword(payload: IForgotPasswordRequest): Observable<never>  {
+  public forgotPassword(payload: IForgotPasswordRequest): Observable<IForgotPasswordDTO> {
     const endpoint = `${this.url}/forgot-password/`;
 
     return this.apiService.post(endpoint, payload);
   }
 
-  public resetPassword(payload: IResetPasswordRequest): Observable<never>  {
+  public resetPassword(payload: IResetPasswordRequest): Observable<never> {
     const endpoint = `${this.url}/reset-password/`;
 
     return this.apiService.post(endpoint, payload);
