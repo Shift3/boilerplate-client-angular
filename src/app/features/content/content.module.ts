9747b446bd012cf4970da0020e22830b8bf00d77
Elanna Grossman <elanna.grossman@gmail.com>
2020-06-12 22:21:55 -0700

    feat(content): add `ContentModule`, wire up `ContentRoutingModule`

---
+++ src/app/features/content/content.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+
+import {
+  ContentRoutingModule,
+  components as contentComponents,
+} from './content-routing.module';
+import { SharedModule } from '@shared/shared.module';
+
+/**
+ * Lazy loaded feature module for all content-related components and dependencies.
+ */
+@NgModule({
+  declarations: [ contentComponents ],
+  imports: [
+    SharedModule,
+    ContentRoutingModule,
+  ],
+})
+export class ContentModule { }
