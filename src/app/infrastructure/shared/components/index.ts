9dc5c6260fa21da788f12c05fc6b6cb97e6e4209
Elanna Grossman <elanna.grossman@gmail.com>
2020-06-15 21:18:08 -0700

    feat(models): add and wire up `TopNavigationComponent`

--- src/app/infrastructure/shared/components/index.ts
+++ src/app/infrastructure/shared/components/index.ts
@@ -2,6 +2,7 @@ import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
 import { FormInputComponent } from './dynamic-form/components/form-input/form-input.component';
 import { FormSelectComponent } from './dynamic-form/components/form-select/form-select.component';
 import { SaveCancelComponent } from './save-cancel/save-cancel.component';
+import { TopNavigationComponent } from './top-navigation/top-navigation.component';
 
 /**
  * Add components that do not need to be specifically referenced.
@@ -11,4 +12,5 @@ export const components = [
   FormInputComponent,
   FormSelectComponent,
   SaveCancelComponent,
+  TopNavigationComponent,
 ];
---
+++ src/app/infrastructure/shared/components/top-navigation/top-navigation.component.ts
@@ -0,0 +1,31 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { INavigation } from '@models/navigation';
+
+@Component({
+  selector: 'app-top-navigation',
+  templateUrl: './top-navigation.component.html',
+  styleUrls: ['./top-navigation.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TopNavigationComponent {
+  public navLinks: INavigation[] = [
+    {
+        id: 1,
+        label: 'Directory',
+        link: '/content',
+        icon: '',
+    },
+    {
+        id: 2,
+        label: 'Users',
+        link: '/admin/user-list',
+        icon: '',
+    },
+    {
+        id: 2,
+        label: 'Settings',
+        link: '/admin/settings',
+        icon: '',
+    },
+];
+}
