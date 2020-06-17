328253c020aea6db33eb3f6e10130ec0ec1f3650
Elanna Grossman <elanna.grossman@gmail.com>
2020-06-16 19:58:51 -0700

    feat(components): add and wire up `ConfirmModalComponent`

--- src/app/infrastructure/shared/components/index.ts
+++ src/app/infrastructure/shared/components/index.ts
@@ -1,3 +1,4 @@
+import { ConfirmModalComponent } from './confirm-modal/confirm-modal.component';
 import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
 import { FormInputComponent } from './dynamic-form/components/form-input/form-input.component';
 import { FormSelectComponent } from './dynamic-form/components/form-select/form-select.component';
@@ -8,6 +9,7 @@ import { TopNavigationComponent } from './top-navigation/top-navigation.componen
  * Add components that do not need to be specifically referenced.
  */
 export const components = [
+  ConfirmModalComponent,
   DynamicFormComponent,
   FormInputComponent,
   FormSelectComponent,
