d49488e88ed3ebf802674040afeb23725c847bd3
Elanna Grossman <elanna.grossman@gmail.com>
2020-06-16 19:57:34 -0700

    feat(models): add `IConfirmModalConfig`

---
+++ src/app/infrastructure/models/modal.ts
@@ -0,0 +1,17 @@
+export interface IConfirmModalConfig {
+  id?: number;
+  message: string;
+  action: string;
+}
+
+export class ConfirmModalConfig implements IConfirmModalConfig {
+  id?: number = 0;
+  message: string = 'Confirm';
+  action: string = 'Confirm';
+
+  constructor(configOverride?: Partial<IConfirmModalConfig>) {
+    if (configOverride) {
+      Object.assign(this, configOverride);
+    }
+  }
+}
