fd6fe5fc98231266bbf648e18b2fd48ad277fd78
Elanna Grossman <elanna.grossman@gmail.com>
2020-07-28 18:25:09 -0700

    feat(user): add `UserLayoutComponent`

---
+++ src/app/features/user/user-layout/user-layout.component.ts
@@ -0,0 +1,26 @@
+import {
+  Component,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+import { NavbarStateService } from '@core/services/navbar-state.service';
+
+/**
+ * Wrapper component for all `UserModule` routes.
+ */
+@Component({
+  templateUrl: './user-layout.component.html',
+  styleUrls: ['./user-layout.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UserLayoutComponent {
+  public navbarToggle$: Observable<string>;
+
+  constructor(
+    private navbarStateService: NavbarStateService,
+  ) {
+    this.navbarToggle$ = this.navbarStateService.getNavbarToggle();
+  }
+}
