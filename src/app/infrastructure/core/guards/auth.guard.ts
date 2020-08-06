0cb9c91706062350301abe6fb2c3ac9df64477bd
Elanna Grossman <elanna.grossman@gmail.com>
2020-08-06 14:57:00 -0700

    feat(guards): add initial implementation of `AuthGuard`

---
+++ src/app/infrastructure/core/guards/auth.guard.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  CanActivateChild,
+  Router,
+} from '@angular/router';
+
+import {
+  Observable,
+  of as observableOf,
+} from 'rxjs';
+
+import { AuthStateService } from '../services/state/auth-state.service';
+import { RoleDTO } from '@models/role';
+import { IUserDTO } from '@models/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate, CanActivateChild {
+  constructor(
+    private authStateService: AuthStateService,
+    private router: Router,
+  ) { }
+
+  public canActivate(): Observable<boolean> {
+    const user: IUserDTO = this.authStateService.getAuthValue();
+    if (RoleDTO.isRoleType(user?.role?.roleName)) {
+      return observableOf(true);
+    }
+
+    this.router.navigateByUrl('/auth');
+    return observableOf(false);
+  }
+
+  public canActivateChild(): Observable<boolean> {
+    return this.canActivate();
+  }
+}
