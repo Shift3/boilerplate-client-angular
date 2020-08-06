7e3be2f6f93cd6e754f00e9639b0c1057955bb64
Elanna Grossman <elanna.grossman@gmail.com>
2020-08-06 14:05:50 -0700

    feat(role): add `IRoleDTO` and `RoleDTO`

--- src/app/infrastructure/models/role.ts
+++ src/app/infrastructure/models/role.ts
@@ -2,6 +2,22 @@ import { ISelectOptions } from './form/select';
 
 export type RoleType = 'Admin' | 'Editor' | 'User' | 'Super Administrator' | '';
 
+export interface IRoleDTO {
+  id: number;
+  roleName: RoleType;
+}
+
+export class RoleDTO implements IRoleDTO {
+  id: number = 0;
+  roleName: RoleType = '';
+
+  constructor(configOverride?: IRoleDTO) {
+    if (configOverride) {
+      Object.assign(this, configOverride);
+    }
+  }
+}
+
 export const roleList: ISelectOptions<RoleType>[] = [
   {
     value: 1,
