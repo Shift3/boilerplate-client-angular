16c28dfb5971ab1bf17969999ba22983629875dc
Elanna Grossman <elanna.grossman@gmail.com>
2020-08-06 14:58:19 -0700

    feat(app): wire up `AuthGuard` and `AdminAuthGuard`

--- src/app/features/app/app-routing.module.ts
+++ src/app/features/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from '@app/infrastructure/core/guards/auth.guard';
+import { AdminAuthGuard } from '@app/infrastructure/core/guards/admin-auth.guard';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
@@ -11,6 +13,8 @@ const routes: Routes = [
   },
   {
     path: 'admin',
+    canActivate: [AdminAuthGuard],
+    canActivateChild: [AdminAuthGuard],
     loadChildren: () => import('../admin/admin.module').then((m) => m.AdminModule),
   },
   {
@@ -19,10 +23,14 @@ const routes: Routes = [
   },
   {
     path: 'content',
+    canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
     loadChildren: () => import('../content/content.module').then((m) => m.ContentModule),
   },
   {
     path: 'user',
+    canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
     loadChildren: () => import('../user/user.module').then((m) => m.UserModule),
   },
   {
