838bba7256c7e8475a15e68781d5074de7bbefbb
Elanna Grossman <elanna.grossman@gmail.com>
2020-08-06 14:06:15 -0700

    feat(role): add type guards as static methods to `RoleDTO`

--- src/app/infrastructure/models/role.ts
+++ src/app/infrastructure/models/role.ts
@@ -11,6 +11,14 @@ export class RoleDTO implements IRoleDTO {
   id: number = 0;
   roleName: RoleType = '';
 
+  static isRoleType(role: RoleType): role is RoleType {
+    return ['Admin', 'Editor', 'User', 'Super Administrator'].includes(role);
+  }
+
+  static isAdminRoleType(role: RoleType): role is RoleType {
+    return ['Admin', 'Super Administrator'].includes(role);
+  }
+
   constructor(configOverride?: IRoleDTO) {
     if (configOverride) {
       Object.assign(this, configOverride);
