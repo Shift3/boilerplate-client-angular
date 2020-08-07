4edb8a27b5916f8319321a30ca05089c14efd03d
Elanna Grossman <elanna.grossman@gmail.com>
2020-08-07 14:49:59 -0700

    test(auth): add tests for `getToken()`, `setToken()`, `resetToken()`

--- src/app/infrastructure/core/services/api/auth.service.spec.ts
+++ src/app/infrastructure/core/services/api/auth.service.spec.ts
@@ -4,6 +4,8 @@ import {
 } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
+import { BehaviorSubject } from 'rxjs';
+
 import { ApiService } from './api.service';
 import { AuthService } from './auth.service';
 import { environment } from '@env/environment.test';
@@ -31,5 +33,36 @@ import { Logger } from '@utils/logger';
     });
     it('should be created', () => {
       expect(service).toBeTruthy();
+    });
+
+    describe('getToken()', () => {
+      it('should return as an Observable', () => {
+        const mockToken$ = new BehaviorSubject<string>('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c');
+        const testToken$ = new BehaviorSubject<string>('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c');
+        const expectedValue = testToken$.asObservable();
+
+        service.token$ = mockToken$;
+        expect(service.getToken()).toEqual(expectedValue);
+      });
+    });
+
+    describe('setToken()', () => {
+      it('should set the emitted value to the token param', () => {
+        const mockToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+        const expectedValue = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+
+        service.setToken(mockToken);
+        expect(service.token$.getValue()).toEqual(expectedValue);
+      });
+    });
+
+    describe('resetToken()', () => {
+      it('should set the emitted value of token$ to empty string', () => {
+        const mockToken$ = new BehaviorSubject<string>('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c');
+
+        service.token$ = mockToken$;
+        service.resetToken();
+        expect(service.token$.getValue()).toEqual('');
+      });
     });
   });
