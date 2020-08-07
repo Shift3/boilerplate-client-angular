60c239024d3ca4e3abf56c8abd73e6100ca81674
Elanna Grossman <elanna.grossman@gmail.com>
2020-08-06 19:52:36 -0700

    tests(auth-state): add tests for `isAdmin()` and `isLoggedInUser()`

--- src/app/infrastructure/core/services/state/auth-state.service.spec.ts
+++ src/app/infrastructure/core/services/state/auth-state.service.spec.ts
@@ -44,4 +44,102 @@ import {
         expect(response).toEqual(expectedValue);
       });
     });
+
+    describe('isAdmin()', () => {
+      it(`should emit false when there is no user session`, () => {
+        service.isAdmin().subscribe((response) => {
+          expect(response).toEqual(false);
+        });
+      });
+
+      it(`should emit false when there is an invalid user`, () => {
+        service.isAdmin().subscribe((response) => {
+          const mockUser$ = new BehaviorSubject<IUserDTO>(new UserDTO());
+
+          service.auth$ = mockUser$;
+          expect(response).toEqual(false);
+        });
+      });
+
+      it(`should emit true when given a role of 'Admin'`, () => {
+        const mockUser$ = new BehaviorSubject<IUserDTO>(new UserDTO({
+          firstName: 'Test',
+          lastName: 'Tester',
+          role: {
+            id: 2,
+            roleName: 'Admin',
+          },
+        }));
+
+        service.auth$ = mockUser$;
+        service.isAdmin().subscribe((response) => {
+          expect(response).toEqual(true);
+        });
+      });
+
+      it(`should emit true when given a role of 'Super Administrator'`, () => {
+        const mockUser$ = new BehaviorSubject<IUserDTO>(new UserDTO({
+          firstName: 'Test',
+          lastName: 'Tester',
+          role: {
+            id: 2,
+            roleName: 'Super Administrator',
+          },
+        }));
+
+        service.auth$ = mockUser$;
+        service.isAdmin().subscribe((response) => {
+          expect(response).toEqual(true);
+        });
+      });
+
+      it(`should emit true when given a non-admin role`, () => {
+        const mockUser$ = new BehaviorSubject<IUserDTO>(new UserDTO({
+          firstName: 'Test',
+          lastName: 'Tester',
+          role: {
+            id: 2,
+            roleName: 'User',
+          },
+        }));
+
+        service.auth$ = mockUser$;
+        service.isAdmin().subscribe((response) => {
+          expect(response).toEqual(false);
+        });
+      });
+    });
+
+    describe('isLoggedInUser()', () => {
+      it(`should emit false when there is no user session`, () => {
+        service.isLoggedInUser().subscribe((response) => {
+          expect(response).toEqual(false);
+        });
+      });
+
+      it(`should emit false when there is an invalid user`, () => {
+        service.isLoggedInUser().subscribe((response) => {
+          const mockUser$ = new BehaviorSubject<IUserDTO>(new UserDTO());
+
+          service.auth$ = mockUser$;
+          expect(response).toEqual(false);
+        });
+      });
+
+      it(`should emit true when given any role`, () => {
+        const mockUser$ = new BehaviorSubject<IUserDTO>(new UserDTO({
+          firstName: 'Test',
+          lastName: 'Tester',
+          role: {
+            id: 2,
+            roleName: 'Editor',
+          },
+        }));
+
+        service.auth$ = mockUser$;
+        service.isLoggedInUser().subscribe((response) => {
+          expect(response).toEqual(true);
+        });
+      });
+    });
   });
