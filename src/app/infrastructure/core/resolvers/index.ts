2c5fc509b7941f017e1e553c8ffd62e8ad4e65ce
Elanna Grossman <elanna.grossman@gmail.com>
2020-08-07 20:10:25 -0700

    feat(resolvers): add and wire up `UpdateUserResolver`

--- src/app/infrastructure/core/resolvers/index.ts
+++ src/app/infrastructure/core/resolvers/index.ts
@@ -1,8 +1,10 @@
 import { CreateUserResolver } from './create-user.resolver';
+import { UpdateUserResolver } from './update-user.resolver';
 
 /**
  * Object of resolvers for individual wiring.
  */
 export const resolverList = {
     CreateUserResolver,
+    UpdateUserResolver,
 };
---
+++ src/app/infrastructure/core/resolvers/update-user.resolver.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import {
+    ActivatedRouteSnapshot,
+    Resolve,
+    RouterStateSnapshot,
+    Router,
+} from '@angular/router';
+
+import {
+  Observable,
+  throwError as observableThrowError,
+} from 'rxjs';
+import {
+  catchError,
+  take,
+} from 'rxjs/operators';
+
+import { IUserDTO } from '@models/user';
+import { UserService } from '../services/api/user.service';
+import { NotificationService } from '../services/notification.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UpdateUserResolver implements Resolve<IUserDTO> {
+  constructor(
+    private notificationService: NotificationService,
+    private router: Router,
+    private userService: UserService,
+  ) { }
+    resolve(route: ActivatedRouteSnapshot, stateSnap: RouterStateSnapshot): Observable<IUserDTO> {
+      const id: number = route.params.id;
+      return this.userService.findUser(id)
+      .pipe(
+        take(1),
+        catchError((error: HttpErrorResponse) => {
+          this.navigateOnError();
+          return observableThrowError(error);
+        }),
+      );
+    }
+
+  private navigateOnError(): void {
+    const message = 'Unable to load user. Returning to user list.';
+    this.notificationService.showError([message]);
+    this.router.navigateByUrl('/admin/user-list');
+  }
+}
