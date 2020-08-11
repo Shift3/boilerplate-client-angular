3fcea8718af10d3bc1d54d3208c70a0fd37de1c7
Elanna Grossman <elanna.grossman@gmail.com>
2020-08-10 19:02:37 -0700

    feat(content-routing): wire up `ProviderTableComponent`, `ProviderListComponent`, set default

--- src/app/features/content/content-routing.module.ts
+++ src/app/features/content/content-routing.module.ts
@@ -2,11 +2,26 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ContentLayoutComponent } from './content-layout/content-layout.component';
+import { ProviderListPresentationComponent } from './providers/provider-list/provider-list-presentation.component';
+import { ProviderListSmartComponent } from './providers/provider-list/provider-list-smart.component';
+import { ProviderTableComponent } from './providers/provider-table/provider-table.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ContentLayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'provider-list',
+        pathMatch: 'full',
+      },
+      {
+        path: 'provider-list',
+        component: ProviderListSmartComponent,
+        data: { title: 'Provider List' },
+      },
+    ],
   },
 ];
 
@@ -21,4 +36,7 @@ export class ContentRoutingModule { }
 
 export const components = [
   ContentLayoutComponent,
+  ProviderListPresentationComponent,
+  ProviderListSmartComponent,
+  ProviderTableComponent,
 ];
