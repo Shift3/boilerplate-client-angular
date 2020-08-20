a55c659770812e335565b895d3ebea9081d69a00
Elanna Grossman <elanna.grossman@gmail.com>
2020-08-19 18:01:48 -0700

    feat(role): add loading and error states to role list

--- src/app/infrastructure/core/services/api/role.service.ts
+++ src/app/infrastructure/core/services/api/role.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@angular/core';
 
-import { Observable } from 'rxjs';
+import {
+  Observable,
+  of as observableOf,
+} from 'rxjs';
+import {
+  catchError,
+  startWith,
+} from 'rxjs/operators';
 
 import { ApiService } from './api.service';
 import { environment } from '@env/environment';
@@ -21,7 +28,24 @@ export class RoleService {
 
   public getRoleList(): Observable<IRoleDTO[]> {
     const endpoint = `${this.url}`;
+    const placeholderRole: IRoleDTO = {
+      id: 0,
+      roleName: 'Loading',
+    };
+    const placeHolderRoleList = new Array<IRoleDTO>();
+    placeHolderRoleList.push(placeholderRole);
 
-    return this.apiService.get<IRoleDTO[]>(endpoint);
+    return this.apiService.get<IRoleDTO[]>(endpoint).pipe(
+      startWith(placeHolderRoleList),
+      catchError(() => {
+        const errorRole: IRoleDTO = {
+          id   : 0,
+          roleName : 'Error Loading Roles',
+        };
+        const errorRoleList = new Array<IRoleDTO>();
+        errorRoleList.push(errorRole);
+        return observableOf(errorRoleList);
+      }),
+    );
   }
 }
