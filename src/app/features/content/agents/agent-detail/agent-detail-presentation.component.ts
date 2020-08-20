2b721d5482ef80d24292fc90822a2698a9c09648
Elanna Grossman <elanna.grossman@gmail.com>
2020-08-19 19:40:51 -0700

    feat(agent-detail): wire up initial dynamic form config

--- src/app/features/content/agents/agent-detail/agent-detail-presentation.component.ts
+++ src/app/features/content/agents/agent-detail/agent-detail-presentation.component.ts
@@ -1,7 +1,15 @@
+
 import {
   ChangeDetectionStrategy,
   Component,
+  EventEmitter,
+  Input,
+  Output,
 } from '@angular/core';
+
+import { IAgentDTO } from '@models/agent';
+import { IFormConfig } from '@models/form/form';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-agent-detail-presentation',
@@ -9,4 +17,20 @@ import {
   styleUrls: ['./agent-detail-presentation.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AgentDetailPresentationComponent { }
+export class AgentDetailPresentationComponent {
+  @Input() public formConfig: IFormConfig;
+  @Input() public formTitle: string = 'Create Agent';
+  @Input() public agent: IAgentDTO;
+
+  @Output() public emitForm = new EventEmitter<FormGroup>();
+  @Output() public emitSubmit = new EventEmitter<void>();
+
+
+  public propagateForm(form: FormGroup): void {
+    this.emitForm.emit(form);
+  }
+
+  public submit(): void {
+    this.emitSubmit.emit();
+  }
+}
