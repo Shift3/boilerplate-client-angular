9f140e73658e3db27ecda46b134809a03303c03f
Elanna Grossman <elanna.grossman@gmail.com>
2020-08-19 19:23:33 -0700

    feat, test(resolvers): add `CreateAgentResolver`, tests

---
+++ src/app/infrastructure/core/resolvers/create-agent.resolver.spec.ts
@@ -0,0 +1,46 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  TestBed,
+  getTestBed,
+} from '@angular/core/testing';
+
+import { AgentDTO } from '@models/agent';
+import { CreateAgentResolver } from './create-agent.resolver';
+import { environment } from '@env/environment.test';
+import { Logger } from '@utils/logger';
+
+!environment.testUnit
+  ? Logger.log('Unit skipped')
+  : describe('[Unit] CreateUserResolver', () => {
+    let resolver: CreateAgentResolver;
+    let injector: TestBed;
+
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        providers: [
+          CreateAgentResolver,
+        ],
+        imports: [HttpClientTestingModule],
+      });
+      injector = getTestBed();
+      resolver = injector.inject(CreateAgentResolver);
+    });
+
+    it('should be created', () => {
+      expect(resolver).toBeTruthy();
+    });
+
+    describe('resolve()', () => {
+      it('should exist', () => {
+        const spy = spyOn(resolver, 'resolve');
+        expect(spy).toBeTruthy();
+      });
+
+      it('should resolve a new instance of the agent object', () => {
+        const expectedValue = new AgentDTO();
+        resolver.resolve().subscribe(response => {
+          expect(response).toEqual(expectedValue);
+        });
+      });
+    });
+  });
