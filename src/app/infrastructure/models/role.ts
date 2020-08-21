e14a1862229f7a5626d3dafe457d419ea60b9d3e
Elanna Grossman <elanna.grossman@gmail.com>
2020-08-21 13:14:16 -0700

    refactor(role): reënable stricter typing

--- src/app/infrastructure/models/role.ts
+++ src/app/infrastructure/models/role.ts
@@ -2,18 +2,18 @@ export type RoleType = 'Admin' | 'Editor' | 'User' | 'Super Administrator' | '';
 
 export interface IRoleDTO {
   id: number;
-  roleName: string;
+  roleName: RoleType;
 }
 
 export class RoleDTO implements IRoleDTO {
   id: number = 0;
-  roleName: string = '';
+  roleName: RoleType = '';
 
-  static isRoleType(role: string): role is RoleType {
+  static isRoleType(role: RoleType): role is RoleType {
     return ['Admin', 'Editor', 'User', 'Super Administrator'].includes(role);
   }
 
-  static isAdminRoleType(role: string): role is RoleType {
+  static isAdminRoleType(role: RoleType): role is RoleType {
     return ['Admin', 'Super Administrator'].includes(role);
   }
 
