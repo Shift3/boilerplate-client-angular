130b5595b8131d652b83bf42d9b1c1a9832cea40
Elanna Grossman <elanna.grossman@gmail.com>
2020-08-24 15:38:14 -0700

    feat(agent-detail-smart): pass values to form, temporarily match name field to mocked response

--- src/app/features/content/agents/agent-detail/agent-detail-smart.component.ts
+++ src/app/features/content/agents/agent-detail/agent-detail-smart.component.ts
@@ -60,21 +60,16 @@ export class AgentDetailSmartComponent implements OnInit  {
       submit: new SaveCancelButtonConfig({save: (this.agent.id) ? 'Update' : 'Create' }),
       controls: [
         new FormField<IInputField>({
-          name: 'firstName',
+          name: 'name',
+          value: this.agent.name,
           fieldType: 'input',
-          label: 'First Name',
+          label: 'Full Name',
           fieldConfig : new InputField(),
-          validation: [ RequiredValidation.required('First Name') ],
-        }),
-        new FormField<IInputField>({
-          name: 'lastName',
-          fieldType: 'input',
-          label: 'Last Name',
-          fieldConfig : new InputField(),
-          validation: [ RequiredValidation.required('Last Name') ],
+          validation: [ RequiredValidation.required('Full Name') ],
         }),
         new FormField<IInputField>({
           name: 'email',
+          value: this.agent.email,
           fieldType: 'input',
           label: 'Email',
           fieldConfig : new InputField({ inputType: 'email' }),
