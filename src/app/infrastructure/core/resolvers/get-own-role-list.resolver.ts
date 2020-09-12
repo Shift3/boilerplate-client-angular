6cc42392c20ad5958c5acf789aeeb1cac7ca50df
Elanna Grossman <elanna.grossman@gmail.com>
2020-09-11 20:04:36 -0700

    feat(resolvers): add and wire up `GetOwnRoleListResolver`

---
+++ src/app/infrastructure/core/resolvers/get-own-role-list.resolver.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import {
+    Resolve,
+    Router,
+} from '@angular/router';
+
+import {
+  Observable,
+  throwError as observableThrowError,
+} from 'rxjs';
+import {
+  catchError,
+  map,
+  mergeMap,
+  take,
+} from 'rxjs/operators';
+
+import { IRoleDTO } from '@models/role';
+import { NotificationService } from '../services/notification.service';
+import { UserService } from '../services/api/user.service';
+import { UserStateService } from '../services/state/user-state.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GetOwnRoleListResolver implements Resolve<IRoleDTO[]> {
+  constructor(
+    private notificationService: NotificationService,
+    private router: Router,
+    private userService: UserService,
+    private userStateService: UserStateService,
+  ) { }
+
+  resolve(): Observable<IRoleDTO[]> {
+    return this.userStateService.getUserSession().pipe(
+      take(1),
+      map((user) => {
+        const roleList = [user.role];
+        return roleList;
+      }),
+      catchError((error: HttpErrorResponse) => {
+        this.navigateOnError();
+        return observableThrowError(error);
+      }),
+    );
+  }
+
+  private navigateOnError(): void {
+    const message = 'Unable to load profile. Returning to dashboard.';
+    this.notificationService.showError([message]);
+    this.router.navigateByUrl('/content/agent-list');
+  }
+}
--- src/app/infrastructure/core/resolvers/index.ts
+++ src/app/infrastructure/core/resolvers/index.ts
@@ -1,6 +1,7 @@
 import { CreateAgentResolver } from './create-agent.resolver';
 import { CreateUserResolver } from './create-user.resolver';
 import { GetRoleListResolver } from './get-role-list.resolver';
+import { GetOwnRoleListResolver } from './get-own-role-list.resolver';
 import { UpdateAgentResolver } from './update-agent.resolver';
 import { UpdateProfileResolver } from './update-profile.resolver';
 import { UpdateUserResolver } from './update-user.resolver';
@@ -12,6 +13,7 @@ export const resolverList = {
   CreateAgentResolver,
   CreateUserResolver,
   GetRoleListResolver,
+  GetOwnRoleListResolver,
   UpdateAgentResolver,
   UpdateProfileResolver,
   UpdateUserResolver,
