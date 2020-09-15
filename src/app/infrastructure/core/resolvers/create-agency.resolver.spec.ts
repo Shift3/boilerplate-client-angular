d032e7e6bbfa2624defbc5ebcaa1a0272689389f
Elanna Grossman <elanna.grossman@gmail.com>
2020-09-14 18:51:59 -0700

    feat, test(resolvers): add and wire up `CreateAgencyResolver`

---
+++ src/app/infrastructure/core/resolvers/create-agency.resolver.spec.ts
@@ -0,0 +1,46 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  TestBed,
+  getTestBed,
+} from '@angular/core/testing';
+
+import { AgencyDTO } from '@models/agency';
+import { CreateAgencyResolver } from './create-agency.resolver';
+import { environment } from '@env/environment.test';
+import { Logger } from '@utils/logger';
+
+!environment.testUnit
+  ? Logger.log('Unit skipped')
+  : describe('[Unit] CreateAgencyResolver', () => {
+    let resolver: CreateAgencyResolver;
+    let injector: TestBed;
+
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        providers: [
+          CreateAgencyResolver,
+        ],
+        imports: [HttpClientTestingModule],
+      });
+      injector = getTestBed();
+      resolver = injector.inject(CreateAgencyResolver);
+    });
+
+    it('should be created', () => {
+      expect(resolver).toBeTruthy();
+    });
+
+    describe('resolve()', () => {
+      it('should exist', () => {
+        const spy = spyOn(resolver, 'resolve');
+        expect(spy).toBeTruthy();
+      });
+
+      it('should resolve a new instance of the agency object', () => {
+        const expectedValue = new AgencyDTO();
+        resolver.resolve().subscribe(response => {
+          expect(response).toEqual(expectedValue);
+        });
+      });
+    });
+  });
--- src/app/infrastructure/core/resolvers/index.ts
+++ src/app/infrastructure/core/resolvers/index.ts
@@ -1,3 +1,4 @@
+import { CreateAgencyResolver } from './create-agency.resolver';
 import { CreateAgentResolver } from './create-agent.resolver';
 import { CreateUserResolver } from './create-user.resolver';
 import { GetRoleListResolver } from './get-role-list.resolver';
@@ -8,6 +9,7 @@ import { UpdateUserResolver } from './update-user.resolver';
  * Object of resolvers for individual wiring.
  */
 export const resolverList = {
+  CreateAgencyResolver,
   CreateAgentResolver,
   CreateUserResolver,
   GetRoleListResolver,
