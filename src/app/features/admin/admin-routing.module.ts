734b8472e788e1f7d890d362a5a609bfc251d71f
Elanna Grossman <elanna.grossman@gmail.com>
2020-09-14 19:11:30 -0700

    feat(admin): wire up `AgencyDetailComponent` routes and resolver

--- src/app/features/admin/admin-routing.module.ts
+++ src/app/features/admin/admin-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
+import { AgencyDetailPresentationComponent } from './agency/agency-detail/agency-detail-presentation.component';
+import { AgencyDetailSmartComponent } from './agency/agency-detail/agency-detail-smart.component';
 import { AgencyTableComponent } from './agency/agency-table/agency-table.component';
 import { UserDetailSmartComponent } from './users/user-detail/user-detail-smart.component';
 import { UserDetailPresentationComponent } from './users/user-detail/user-detail-presentation.component';
@@ -44,6 +46,22 @@ const routes: Routes = [
         component: UserListSmartComponent,
         data: { title: 'User List' },
       },
+      {
+        path: 'create-agency',
+        component: AgencyDetailSmartComponent,
+        data: { title: 'Add Agency' },
+        resolve: {
+          agency: resolverList.CreateAgencyResolver,
+        },
+      },
+      {
+        path: 'update-agency/:id',
+        component: AgencyDetailSmartComponent,
+        data: { title: 'Update Agency' },
+        resolve: {
+          agency: resolverList.UpdateAgencyResolver,
+        },
+      },
     ],
   },
 ];
@@ -56,6 +74,8 @@ export class AdminRoutingModule { }
 
 export const components = [
   AdminLayoutComponent,
+  AgencyDetailSmartComponent,
+  AgencyDetailPresentationComponent,
   UserDetailPresentationComponent,
   UserDetailSmartComponent,
   AgencyTableComponent,
