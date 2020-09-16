b92a072b4c829c348a6c4eae92768599d93075f0
Elanna Grossman <elanna.grossman@gmail.com>
2020-09-15 18:41:38 -0700

    feat(resolvers): add and wire up `GetAgencyListResolver`

---
+++ src/app/infrastructure/core/resolvers/get-agency-list.resolver.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import {
+  Resolve,
+  Router,
+} from '@angular/router';
+
+import {
+  Observable,
+  throwError as observableThrowError,
+} from 'rxjs';
+import {
+  catchError,
+  map,
+  mergeMap,
+  take,
+} from 'rxjs/operators';
+
+import { AgencyService } from '../services/api/agency.service';
+import { IAgencyDTO } from '@models/agency';
+import { NotificationService } from '../services/notification.service';
+import { UserService } from '../services/api/user.service';
+import { UserStateService } from '../services/state/user-state.service';
+
+/**
+ * Returns the server-provided list of agencies for Super Administrators, or the assigned agency for Admins.
+ */
+@Injectable({
+  providedIn: 'root',
+})
+export class GetAgencyListResolver implements Resolve<IAgencyDTO[]> {
+  constructor(
+    private notificationService: NotificationService,
+    private router: Router,
+    private agencyService: AgencyService,
+    private userService: UserService,
+    private userStateService: UserStateService,
+  ) { }
+
+  resolve(): Observable<IAgencyDTO[]> {
+    return this.userStateService.checkRoleGuard()
+      .pipe(
+        take(1),
+        mergeMap((roleGuard) => {
+          if (roleGuard.isSuperAdmin) {
+            return this.agencyService.getAgencyList();
+          } else {
+            return this.userStateService.getUserSession().pipe(
+              take(1),
+              mergeMap((loggedInUser) => this.userService.findUser(loggedInUser.id)),
+              map(user => [user.agency]),
+            );
+          }
+        }),
+        catchError((error: HttpErrorResponse) => {
+          this.navigateOnError();
+          return observableThrowError(error);
+        }),
+      );
+  }
+
+  private navigateOnError(): void {
+    const message = 'Unable to load agencies. Returning to user list.';
+    this.notificationService.showError([message]);
+    this.router.navigateByUrl('/admin/user-list');
+  }
+}
--- src/app/infrastructure/core/resolvers/index.ts
+++ src/app/infrastructure/core/resolvers/index.ts
@@ -1,6 +1,7 @@
 import { CreateAgencyResolver } from './create-agency.resolver';
 import { CreateAgentResolver } from './create-agent.resolver';
 import { CreateUserResolver } from './create-user.resolver';
+import { GetAgencyListResolver } from './get-agency-list.resolver';
 import { GetRoleListResolver } from './get-role-list.resolver';
 import { UpdateAgencyResolver } from './update-agency.resolver';
 import { UpdateAgentResolver } from './update-agent.resolver';
@@ -13,6 +14,7 @@ export const resolverList = {
   CreateAgencyResolver,
   CreateAgentResolver,
   CreateUserResolver,
+  GetAgencyListResolver,
   GetRoleListResolver,
   UpdateAgencyResolver,
   UpdateAgentResolver,
