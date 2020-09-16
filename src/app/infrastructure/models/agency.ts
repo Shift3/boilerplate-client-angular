2793d2939b56fce4b76ce2192f6ad9edcbf7a38b
Elanna Grossman <elanna.grossman@gmail.com>
2020-09-15 17:22:25 -0700

    refactor(agency): update nullable properties

--- src/app/infrastructure/models/agency.ts
+++ src/app/infrastructure/models/agency.ts
@@ -1,16 +1,16 @@
 export interface IAgencyDTO {
   id: number;
-  createdBy: number;
-  deletedAt: string;
-  deletedBy: number;
+  createdBy?: number;
+  deletedAt?: string;
+  deletedBy?: number;
   agencyName: string;
 }
 
 export class AgencyDTO implements IAgencyDTO {
   id: number = 0;
-  createdBy: number = 0;
-  deletedAt: string = '';
-  deletedBy: number = 0;
+  createdBy?: number = 0;
+  deletedAt?: string = '';
+  deletedBy?: number = 0;
   agencyName: string = '';
 
   constructor(configOverride?: Partial<IAgencyDTO>) {
