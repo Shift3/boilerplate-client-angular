081b97f7f2b43630efb395b735ce29740c607082
Elanna Grossman <elanna.grossman@gmail.com>
2020-09-17 15:24:33 -0700

    feat(footer): bind calculated `copyrightYear` to template

--- src/app/infrastructure/shared/components/footer/footer.component.ts
+++ src/app/infrastructure/shared/components/footer/footer.component.ts
@@ -3,10 +3,14 @@ import {
   ChangeDetectionStrategy,
 } from '@angular/core';
 
+import { Utils } from '@utils/utils';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FooterComponent { }
+export class FooterComponent {
+  public copyrightYear: string = Utils.getCopyrightYear();
+}
