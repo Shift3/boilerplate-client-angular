6382e0f895d6ad04ed3501d49c315eb2c151f5be
Elanna Grossman <elanna.grossman@gmail.com>
2020-09-18 12:06:32 -0700

    feat(notification): inject `ToastrService` and use instead of alerts

--- src/app/infrastructure/core/services/notification.service.ts
+++ src/app/infrastructure/core/services/notification.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
 
+import { ToastrService } from 'ngx-toastr';
+
 @Injectable({
   providedIn: 'root',
 })
 export class NotificationService {
-  public showSuccess(message: string[]): void {
-    alert(message);
+  constructor(
+    private toastr: ToastrService,
+  ) { }
+  public showSuccess(messageList: string[]): void {
+    this.toastr.success(messageList[0]);
   }
 
-  public showError(message: string[]): void {
-    alert(message);
+  public showError(messageList: string[]): void {
+    this.toastr.error(messageList[0]);
   }
 }
