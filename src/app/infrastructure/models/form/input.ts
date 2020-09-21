6cb2fd847f909a5193568732a270eb4c10f40b00
Elanna Grossman <elanna.grossman@gmail.com>
2020-09-21 12:29:08 -0700

    feat(input): add mask property to `IInputField`

--- src/app/infrastructure/models/form/input.ts
+++ src/app/infrastructure/models/form/input.ts
@@ -5,11 +5,13 @@ export type InputType = 'text' | 'email' | 'password' | 'number';
 export interface IInputField {
   inputType: InputType;
   autocomplete: AutocompleteType;
+  mask: string;
 }
 
 export class InputField implements IInputField {
   inputType: InputType = 'text';
   autocomplete: AutocompleteType = 'on';
+  mask: string = '';
 
   constructor(configOverride?: Partial<IInputField>) {
     if (configOverride) {
