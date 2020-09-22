6df770b420068659797db81832d8c2a2f6ca3398
Elanna Grossman <elanna.grossman@gmail.com>
2020-09-22 01:56:12 -0700

    test(error): add test for `getClientMessage()`

--- src/app/infrastructure/core/services/error.service.spec.ts
+++ src/app/infrastructure/core/services/error.service.spec.ts
@@ -24,16 +24,24 @@ import { Logger } from '@utils/logger';
     });
 
     describe('getClientMessage()', () => {
-      it('should return the passed in error message in a list', () => {
+      it(`should return the passed in error message in a list`, () => {
         const error = new Error('Test');
         const expectedValue = ['Test'];
+
+        expect(service.getClientMessage(error)).toEqual(expectedValue);
+      });
+
+      it(`should return 'No Internet Connection.' in a list when the client reports being offline`, () => {
+        const error = new Error('Test');
+        const expectedValue = ['No Internet Connection.'];
+        spyOnProperty(navigator, 'onLine').and.returnValue(false);
 
         expect(service.getClientMessage(error)).toEqual(expectedValue);
       });
     });
 
     describe('getServerMessage()', () => {
-      it('should return the passed in error message in a list', () => {
+      it(`should return the passed in error message in a list`, () => {
         const error = new Error('Test');
         const httpError = new HttpErrorResponse({error});
         const expectedValue = ['Test'];
