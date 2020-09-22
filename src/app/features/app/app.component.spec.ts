179f49fa6769e7359718bf464f1533dbb56ccc9d
Elanna Grossman <elanna.grossman@gmail.com>
2020-09-22 04:49:16 -0700

    test(app): use ng-mocks for component dependencies

--- src/app/features/app/app.component.spec.ts
+++ src/app/features/app/app.component.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
+import { MockComponent } from 'ng-mocks';
+
 import { AppComponent } from './app.component';
 import { environment } from '@env/environment.test';
 import { Logger } from '@utils/logger';
 import { FooterComponent } from '@shared/components/footer/footer.component';
-import { SideNavigationPresentationComponent } from '@shared/components/navigation/side-navigation/side-navigation-presentation.component';
 import { SideNavigationSmartComponent } from '@shared/components/navigation/side-navigation/side-navigation-smart.component';
-import { TopNavigationPresentationComponent } from '@shared/components/navigation/top-navigation/top-navigation-presentation.component';
 import { TopNavigationSmartComponent } from '@shared/components/navigation/top-navigation/top-navigation-smart.component';
 
 !environment.testIntegration
@@ -20,11 +20,9 @@ import { TopNavigationSmartComponent } from '@shared/components/navigation/top-n
         ],
         declarations: [
           AppComponent,
-          FooterComponent,
-          SideNavigationPresentationComponent,
-          SideNavigationSmartComponent,
-          TopNavigationPresentationComponent,
-          TopNavigationSmartComponent,
+          MockComponent(FooterComponent),
+          MockComponent(SideNavigationSmartComponent),
+          MockComponent(TopNavigationSmartComponent),
         ],
       }).compileComponents();
     }));
