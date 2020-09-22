7e6c98552617a5c6611f97f61d738bc5f25496b3
Elanna Grossman <elanna.grossman@gmail.com>
2020-09-22 04:49:34 -0700

    refactor(side-navigation): remove unused import

--- src/app/infrastructure/shared/components/navigation/side-navigation/side-navigation-presentation.component.spec.ts
+++ src/app/infrastructure/shared/components/navigation/side-navigation/side-navigation-presentation.component.spec.ts
@@ -3,7 +3,6 @@ import {
   ComponentFixture,
   TestBed,
 } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { MockComponent } from 'ng-mocks';
