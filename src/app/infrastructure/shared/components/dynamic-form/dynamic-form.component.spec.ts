1193450010e94bf6a17ff10a1e104b54505c81c1
Elanna Grossman <elanna.grossman@gmail.com>
2020-09-22 03:01:44 -0700

    test(dynamic-form): add tests for `DynamicFormComponent`

---
+++ src/app/infrastructure/shared/components/dynamic-form/dynamic-form.component.spec.ts
@@ -0,0 +1,41 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { MockComponent } from 'ng-mocks';
+
+import { DynamicFormComponent } from './dynamic-form.component';
+import { FormBuilder } from '@angular/forms';
+import { SaveCancelComponent } from '../save-cancel/save-cancel.component';
+
+describe('DynamicFormComponent', () => {
+  let component: DynamicFormComponent;
+  let fixture: ComponentFixture<DynamicFormComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        DynamicFormComponent,
+        MockComponent(SaveCancelComponent),
+      ],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+      ],
+      providers: [
+        FormBuilder,
+      ],
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DynamicFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
