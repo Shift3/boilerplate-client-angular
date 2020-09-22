40538d7f9781808eec244223a2978868f7206eb6
Elanna Grossman <elanna.grossman@gmail.com>
2020-09-22 01:52:11 -0700

    test(error): add tests for `ErrorService`

---
+++ src/app/infrastructure/core/services/error.service.spec.ts
@@ -0,0 +1,44 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
+
+import { environment } from '@env/environment.test';
+import { ErrorService } from './error.service';
+import { Logger } from '@utils/logger';
+
+!environment.testUnit
+  ? Logger.log('Unit skipped')
+  : describe('[Unit] ErrorService', () => {
+    let service: ErrorService;
+
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        providers: [
+          ErrorService,
+        ],
+      });
+      service = TestBed.inject(ErrorService);
+    });
+
+    it('should be created', () => {
+      expect(service).toBeTruthy();
+    });
+
+    describe('getClientMessage()', () => {
+      it('should return the passed in error message in a list', () => {
+        const error = new Error('Test');
+        const expectedValue = ['Test'];
+
+        expect(service.getClientMessage(error)).toEqual(expectedValue);
+      });
+    });
+
+    describe('getServerMessage()', () => {
+      it('should return the passed in error message in a list', () => {
+        const error = new Error('Test');
+        const httpError = new HttpErrorResponse({error});
+        const expectedValue = ['Test'];
+
+        expect(service.getServerMessage(httpError)).toEqual(expectedValue);
+      });
+    });
+  });
