5d3a2b32e561eb720fdacae18463aa6bd2cb0d7a
Elanna Grossman <elanna.grossman@gmail.com>
2020-09-22 01:00:01 -0700

    test(): add tests for `checkRoleList()`

--- src/app/infrastructure/core/services/state/user-state.service.spec.ts
+++ src/app/infrastructure/core/services/state/user-state.service.spec.ts
@@ -4,6 +4,7 @@ import { BehaviorSubject } from 'rxjs';
 
 import { environment } from '@env/environment.test';
 import { Logger } from '@utils/logger';
+import { RoleCheck } from '@models/role';
 import {
   IUserDTO,
   UserDTO,
@@ -118,6 +119,109 @@ import { UserStateService } from './user-state.service';
         service.userSession$ = testUser$;
         service.resetUserSession();
         expect(service.userSession$.getValue()).toEqual(null);
+      });
+    });
+
+    describe('checkRoleList()', () => {
+      it(`should return a role object with all false values when there is no user session`, () => {
+        const expectedValue = new RoleCheck();
+
+        service.checkRoleList().subscribe((response) => {
+          expect(response).toEqual(expectedValue);
+        });
+      });
+
+      it(`should return a role object with all false values when there is an invalid user`, () => {
+        service.checkRoleList().subscribe((response) => {
+          const testUser$ = new BehaviorSubject<IUserDTO>(new UserDTO());
+          const expectedValue = new RoleCheck();
+
+          service.userSession$ = testUser$;
+          expect(response).toEqual(expectedValue);
+        });
+      });
+
+      it(`should return a role object with a true value for 'isAuthenticated' when given any role`, () => {
+        const testUser$ = new BehaviorSubject<IUserDTO>(new UserDTO({
+          firstName: 'Test',
+          lastName: 'Tester',
+          role: {
+            id: 3,
+            roleName: 'User',
+          },
+        }));
+        const expectedValue = new RoleCheck({ isAuthenticated: true });
+
+        service.userSession$ = testUser$;
+        service.checkRoleList().subscribe((response) => {
+          expect(response).toEqual(expectedValue);
+        });
+      });
+
+      it(`should return a role object with true values for 'isAuthenticated' and 'canEdit' when given an 'Editor' role`, () => {
+        const testUser$ = new BehaviorSubject<IUserDTO>(new UserDTO({
+          firstName: 'Test',
+          lastName: 'Tester',
+          role: {
+            id: 3,
+            roleName: 'Editor',
+          },
+        }));
+        const expectedValue = new RoleCheck({
+          isAuthenticated: true,
+          canEdit: true,
+          isAdmin: false,
+          isSuperAdmin: false,
+        });
+
+        service.userSession$ = testUser$;
+        service.checkRoleList().subscribe((response) => {
+          expect(response).toEqual(expectedValue);
+        });
+      });
+
+      it(`should return a role object with true values for 'isAuthenticated' and 'canEdit', and 'isAdmin' when given an 'Admin' role`, () => {
+        const testUser$ = new BehaviorSubject<IUserDTO>(new UserDTO({
+          firstName: 'Test',
+          lastName: 'Tester',
+          role: {
+            id: 3,
+            roleName: 'Admin',
+          },
+        }));
+        const expectedValue = new RoleCheck({
+          isAuthenticated: true,
+          canEdit: true,
+          isAdmin: true,
+          isSuperAdmin: false,
+        });
+
+        service.userSession$ = testUser$;
+        service.checkRoleList().subscribe((response) => {
+          expect(response).toEqual(expectedValue);
+        });
+      });
+
+      it(`should return a role object with all true values when given a 'Super Administrator' role`, () => {
+        const testUser$ = new BehaviorSubject<IUserDTO>(new UserDTO({
+          firstName: 'Test',
+          lastName: 'Tester',
+          role: {
+            id: 3,
+            roleName: 'Super Administrator',
+          },
+        }));
+        const expectedValue = new RoleCheck({
+          isAuthenticated: true,
+          canEdit: true,
+          isAdmin: true,
+          isSuperAdmin: true,
+        });
+
+        service.userSession$ = testUser$;
+        service.checkRoleList().subscribe((response) => {
+          expect(response).toEqual(expectedValue);
+        });
       });
     });
   });
