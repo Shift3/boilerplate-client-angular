593f078439ea01351e2df2a681f0e93def0195de
Elanna Grossman <elanna.grossman@gmail.com>
2020-09-29 15:07:40 -0700

    feat(track-by-key): rename warning for clarity

--- src/app/infrastructure/shared/pipes/track-by-key.pipe.ts
+++ src/app/infrastructure/shared/pipes/track-by-key.pipe.ts
@@ -8,9 +8,9 @@ import { Logger } from '@utils/logger';
 @Pipe({ name: 'trackByKey' })
 export class TrackByKeyPipe implements PipeTransform {
   public transform(key: string): <T, U>(index: number, value: T) => U | null {
-    return function trackByProperty<T, U>(index: number, value: T): U | null {
+    return function trackByKey<T, U>(index: number, value: T): U | null {
       if (value[key] === undefined) {
-        Logger.warn('Key not provided to trackByValue() for *ngFor.');
+        Logger.warn('Key not provided to trackByKey for *ngFor.');
       }
 
       return (value) ? value[key] as U : null;
