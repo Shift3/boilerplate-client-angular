1a937830caed97123ed0d2b0c8309dc1dc2635a2
Elanna Grossman <elanna.grossman@gmail.com>
2020-10-29 12:48:51 -0700

    test(redirect): improve test description

--- src/app/redirect/redirect.component.spec.ts
+++ src/app/redirect/redirect.component.spec.ts
@@ -1,24 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { environment } from '@env/environment.test';
+import { Logger } from '@utils/logger';
 import { RedirectComponent } from './redirect.component';
 
-describe('RedirectComponent', () => {
-  let component: RedirectComponent;
-  let fixture: ComponentFixture<RedirectComponent>;
+!environment.testIntegration
+  ? Logger.log('Integration skipped')
+  : describe('[Integration] RedirectComponent', () => {
+      let component: RedirectComponent;
+      let fixture: ComponentFixture<RedirectComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [RedirectComponent],
-    }).compileComponents();
-  }));
+      beforeEach(async(() => {
+        TestBed.configureTestingModule({
+          declarations: [RedirectComponent],
+        }).compileComponents();
+      }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(RedirectComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+      beforeEach(() => {
+        fixture = TestBed.createComponent(RedirectComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+      });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+      it('should create', () => {
+        expect(component).toBeTruthy();
+      });
+    });
