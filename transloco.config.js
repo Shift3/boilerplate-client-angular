889da936b119bb39d756f2cc3cf701d3b68e86a4
Pratima Sakinala <pratimasakinala13@gmail.com>
2021-02-22 09:38:39 -0800

    refactor(transloco-config, api.service): moves keysManager and rootTranslationsPath properties from transloco.config into transloco-config. removes transloco.config. uses rootTranslationsPath property to get the path in ApiService getTranslations function.

--- transloco.config.js
+++
@@ -1,7 +0,0 @@
-import { translocoConfigObj } from '@app/transloco/transloco-config';
-
-module.exports = {
-  rootTranslationsPath: 'src/assets/i18n/',
-  langs: translocoConfigObj.availabeLangs,
-  keysManager: {},
-};
