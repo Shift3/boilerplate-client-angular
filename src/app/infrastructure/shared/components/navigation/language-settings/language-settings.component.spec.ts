365bf48d5aa3af9da74297dd69085b34d03d4a3b
Pratima Sakinala <pratimasakinala13@gmail.com>
2021-03-02 22:06:30 -0800

    test(language-settings): adds basic should create test for the language-settings component

--- src/app/infrastructure/shared/components/navigation/language-settings/language-settings.component.spec.ts
+++ src/app/infrastructure/shared/components/navigation/language-settings/language-settings.component.spec.ts
@@ -1,23 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TranslocoTestingModule } from '@ngneat/transloco';
 import { LanguageSettingsComponent } from './language-settings.component';
-
 describe('LanguageSettingsComponent', () => {
   let component: LanguageSettingsComponent;
   let fixture: ComponentFixture<LanguageSettingsComponent>;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [LanguageSettingsComponent],
+      imports: [TranslocoTestingModule],
     }).compileComponents();
   }));
-
   beforeEach(() => {
     fixture = TestBed.createComponent(LanguageSettingsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
   it('should create', () => {
     expect(component).toBeTruthy();
   });
