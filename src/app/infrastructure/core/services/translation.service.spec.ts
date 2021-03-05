7fd8ed9ee2da4b500b6a2acbe57c1e25cb1fb654
Pratima Sakinala <pratimasakinala13@gmail.com>
2021-03-04 22:14:09 -0800

    test(services, components): adds TranslocoTestingModule to navigation and settings component test files. fixes selectLanguage test in language-state.service.spec. Adds proper tests to translation.service.spec

--- src/app/infrastructure/core/services/translation.service.spec.ts
+++ src/app/infrastructure/core/services/translation.service.spec.ts
@@ -1,8 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 
+import { TranslocoTestingModule } from '@ngneat/transloco';
+
 import { environment } from '@env/environment.test';
 import { Logger } from '@utils/logger';
 import { TranslationService } from '@core/services/translation.service';
+import { LanguageStateService } from '@core/services/state/language-state.service';
 
 !environment.testUnit
   ? Logger.log('Unit skipped')
@@ -12,10 +15,17 @@ import { TranslationService } from '@core/services/translation.service';
         profile: 'userProfile.profileLinks.profile',
         changePassword: 'changePassword',
       };
+      const languageStateMock = {
+        getTextInDefaultLang: jasmine.createSpy('getTextInDefaultLang'),
+      };
 
       beforeEach(() => {
         TestBed.configureTestingModule({
-          providers: [TranslationService],
+          imports: [TranslocoTestingModule],
+          providers: [
+            TranslationService,
+            { provide: LanguageStateService, useValue: languageStateMock },
+          ],
         });
         service = TestBed.inject(TranslationService);
       });
@@ -25,9 +35,31 @@ import { TranslationService } from '@core/services/translation.service';
       });
 
       describe('getObjectProperty()', () => {
+        it('should return blank string for empty label', () => {
+          const constructedString = service.getObjectProperty(
+            'userProfile.profileLinks',
+            '',
+          );
+
+          expect(constructedString).toEqual('');
+        });
+
+        it('should return blank string for null label', () => {
+          const constructedString = service.getObjectProperty(
+            'userProfile.profileLinks',
+            null,
+          );
+
+          expect(constructedString).toEqual('');
+        });
+
         it('should return translation object property to be read from the JSON files', () => {
-          service.getObjectProperty('userProfile.profileLinks', 'profile');
-          expect(translationMock.profile);
+          const constructedString = service.getObjectProperty(
+            'userProfile.profileLinks',
+            'profile',
+          );
+
+          expect(constructedString).toEqual(translationMock.profile);
         });
       });
     });
