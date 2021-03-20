baf0f9e52b064d52178c8dbc61c89b969fe862f1
Pratima Sakinala <pratimasakinala13@gmail.com>
2021-03-19 17:49:58 -0700

    fix(error.service): removes the type: `dynamic` value from getServerMessage test

--- src/app/infrastructure/core/services/error.service.spec.ts
+++ src/app/infrastructure/core/services/error.service.spec.ts
@@ -48,9 +48,7 @@ import { SentryConfig } from '@models/error';
         it(`should return the passed in error message in a list`, () => {
           const error = new Error('Test');
           const httpError = new HttpErrorResponse({ error });
-          const expectedValue = [
-            new Message({ type: 'dynamic', message: 'Test' }),
-          ];
+          const expectedValue = [new Message({ message: 'Test' })];
 
           expect(service.getServerMessage(httpError)).toEqual(expectedValue);
         });
