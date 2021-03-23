ca5e37fa1b4ecddb67554edfdcbe65f46275b67f
Pratima Sakinala <pratimasakinala13@gmail.com>
2021-03-23 06:17:53 -0700

    refactor(translation, dynamic-table): creates a new file to store interfaces & classes for all keys under `dynamicTable` from the JSON files. Adds `dynamicTable` to ITranslation interface.

--- src/app/infrastructure/models/translation/translation.ts
+++ src/app/infrastructure/models/translation/translation.ts
@@ -2,9 +2,11 @@ import { IConfirmationModal } from './confirmation-modal';
 import { INotFound } from './not-found';
 import { INotification } from './notification';
 import { ISignUp } from './sign-up';
+import { IDynamicTable } from './dynamic-table';
 
 export interface ITranslation {
   confirmationModal: IConfirmationModal;
+  dynamicTable: IDynamicTable;
   notFound: INotFound;
   notification: INotification;
   signUp: ISignUp;
