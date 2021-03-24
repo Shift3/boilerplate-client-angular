1e5cf2f52a172a56e611ab0c3c94d20f95224da2
Elanna Grossman <elanna.grossman@gmail.com>
2021-03-23 21:50:35 -0700

    feat(models): add `shouldOptionNameBeTranslatedByClient` property

--- src/app/infrastructure/models/form/select.ts
+++ src/app/infrastructure/models/form/select.ts
@@ -18,12 +18,14 @@ export interface ISelectField<T> {
   options: ISelectOptions<T>[];
   optionName: string;
   optionValue: string;
+  shouldOptionNameBeTranslatedByClient: boolean;
 }
 
 export class SelectField<T> implements ISelectField<T> {
   options: ISelectOptions<T>[] = [];
   optionName: string = 'name';
   optionValue: string = 'value';
+  shouldOptionNameBeTranslatedByClient: boolean = false;
 
   constructor(configOverride?: Partial<ISelectField<T>>) {
     if (configOverride) {
