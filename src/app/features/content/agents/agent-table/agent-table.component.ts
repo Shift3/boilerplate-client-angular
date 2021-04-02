2881f5d4d0c4669f6459cd9a2a722d7899d519a7
Elanna Grossman <elanna.grossman@gmail.com>
2021-04-01 19:56:51 -0700

    feat(agent-table): wire up popover template

--- src/app/features/content/agents/agent-table/agent-table.component.ts
+++ src/app/features/content/agents/agent-table/agent-table.component.ts
@@ -26,10 +26,15 @@ export class AgentTableComponent {
   @Input() public tableData: IAgentDTO[] = [];
 
   @Output() public emitDelete = new EventEmitter<IAgentDTO>();
+  @Output() public emitSelectLanguage = new EventEmitter<string>();
 
   public dynamicTableTranslationKeys: IDynamicTableTranslationKey = new DynamicTableTranslationKey();
 
   public deleteAgent(agent: IAgentDTO): void {
     this.emitDelete.emit(agent);
+  }
+
+  public selectLanguage(languageCode: string): void {
+    this.emitSelectLanguage.emit(languageCode);
   }
 }
