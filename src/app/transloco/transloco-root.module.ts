571d0032d8a31b35f54d0a94f2a798c0ce85a1ae
Pratima Sakinala <pratimasakinala13@gmail.com>
2021-04-19 22:26:29 -0700

    refactor(transloco-root.module): Adds logic to prefetch the User Language using APP_INITIALIZER in the Transloco-root-module.

--- src/app/transloco/transloco-root.module.ts
+++ src/app/transloco/transloco-root.module.ts
@@ -1,4 +1,4 @@
-import { Injectable, NgModule } from '@angular/core';
+import { Injectable, NgModule, APP_INITIALIZER } from '@angular/core';
 
 import {
   TRANSLOCO_LOADER,
@@ -6,15 +6,19 @@ import {
   TRANSLOCO_CONFIG,
   translocoConfig,
   TranslocoModule,
+  TranslocoService,
 } from '@ngneat/transloco';
 import { TranslocoLocaleModule } from '@ngneat/transloco-locale';
 import {
   TranslocoPersistLangModule,
   TRANSLOCO_PERSIST_LANG_STORAGE,
 } from '@ngneat/transloco-persist-lang';
+import { tap } from 'rxjs/operators';
 
 import { ApiService } from '@core/services/api/api.service';
 import { translocoConfigObj } from './transloco-config';
+import { UserDTO } from '@models/user';
+import { UserStateService } from '@core/services/state/user-state.service';
 
 @Injectable({ providedIn: 'root' })
 export class TranslocoHttpLoader implements TranslocoLoader {
@@ -23,6 +27,21 @@ export class TranslocoHttpLoader implements TranslocoLoader {
   getTranslation(lang: string) {
     return this.apiService.getTranslation(lang);
   }
+}
+
+export function preloadUserLanguage(
+  userStateService: UserStateService,
+  transloco: TranslocoService,
+) {
+  return () => {
+    return userStateService.getUserSession().pipe(
+      tap((user: UserDTO) => {
+        console.log(user);
+        transloco.setActiveLang(user.settings.language.languageCode);
+        return transloco.load(user.settings.language.languageCode).toPromise();
+      }),
+    );
+  };
 }
 
 @NgModule({
@@ -46,6 +65,12 @@ export class TranslocoHttpLoader implements TranslocoLoader {
     {
       provide: TRANSLOCO_LOADER,
       useClass: TranslocoHttpLoader,
+    },
+    {
+      provide: APP_INITIALIZER,
+      multi: true,
+      useFactory: preloadUserLanguage,
+      deps: [UserStateService, TranslocoService],
     },
   ],
 })
