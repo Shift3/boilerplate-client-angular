34b6c4d648869cd1cafc4e9a2eb2c6daa9edeb40
Elanna Grossman <elanna.grossman@gmail.com>
2021-06-23 17:48:45 -0700

    fix(app): remove inert function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule, APP_INITIALIZER } from '@angular/core';
+import { NgModule } from '@angular/core';
 
-import { tap } from 'rxjs/operators';
 import { ToastrModule } from 'ngx-toastr';
-import { TranslocoService } from '@ngneat/transloco';
 
 import {
   AppRoutingModule,
@@ -13,29 +11,7 @@ import {
 import { AppComponent } from './app.component';
 import { CoreModule } from '@core/core.module';
 import { SharedModule } from '@shared/shared.module';
-import { translocoConfigObj } from '@app/transloco/transloco-config';
 import { TranslocoRootModule } from '@app/transloco/transloco-root.module';
-import { IUserDTO } from '@models/user';
-import { UserStateService } from '@core/services/state/user-state.service';
-
-export function preloadUserSettings(
-  userStateService: UserStateService,
-  transloco: TranslocoService,
-) {
-  return () => {
-    return userStateService.getUserSession().pipe(
-      tap((user: IUserDTO) => {
-        userStateService.setUserSettings(user.settings);
-
-        const preferredLang: string =
-          user.settings.language?.languageCode ||
-          translocoConfigObj.defaultLang;
-
-        return transloco.load(preferredLang).toPromise();
-      }),
-    );
-  };
-}
 
 /**
  * Root feature module.
@@ -54,14 +30,6 @@ export function preloadUserSettings(
     TranslocoRootModule,
     // AppRoutingModule must be loaded last.
     AppRoutingModule,
-  ],
-  providers: [
-    {
-      provide: APP_INITIALIZER,
-      multi: true,
-      useFactory: preloadUserSettings,
-      deps: [UserStateService, TranslocoService],
-    },
   ],
   bootstrap: [AppComponent],
 })
